7295c7c3a5d8369e7391ec2f5d4f54d2c491ae84
Joakim Brorsson <b.joakim@gmail.com>
2023-09-17 14:46:18 +0200

    cleanup

--- OpenSSL-for-iOS/SSS.c
+++ OpenSSL-for-iOS/SSS.c
@@ -19,57 +19,45 @@ void genShamirShares(EC_POINT **shares, EC_POINT *secret, const int t, const int
         coeffs[i] = randZp(ctx);
     }
     
-    //debug
-    printf("coeffs:\n");
-    for (int i = 0; i < t + 1; i++) {
-        printBN(coeffs[i]);
-    }
-    
     BIGNUM *peval = BN_new();//space for evaluating polynomial
     BIGNUM *pterm = BN_new();//space for storing polynomial terms
     BIGNUM *base = BN_new();//space for storing polynomial terms
     BIGNUM *exp = BN_new();//space for storing polynomial terms
     //make shares for user i, counting starts from 1, not 0
     for (int i = 1; i <= n; i++){
+        
         BN_set_word(peval, 0);//reset space for reuse
+        
         //evaluate polynomial
         for (int j = 0; j < t + 1; j++) { //coeff * i ** j
+            
             BN_set_word(base, i);
             BN_set_word(exp, j);
             BN_mod_exp(pterm, base, exp, get0Order(), ctx); // i**j
             BN_mod_mul(pterm, coeffs[j], pterm, get0Order(), ctx); //coeff * (i ** j)
             BN_mod_add(peval, peval, pterm, get0Order(), ctx); // add term to po;ly eval
-//            printf("adding term: \n");
-//            printBN(coeffs[j]);
-//            printf(" * ");
-//            printBN(base);
-//            printf(" ** ");
-//            printBN(exp);
-//            printf(" = ");
-//            printBN(pterm);
             
         }
         
-//        printf("decimal share: ");
-//        printBN(peval);
-//        printf("pointshare: ");
         shares[i - 1] = multiply(get0Gen(), peval);
         EC_POINT_add(get0Group(), shares[i - 1], shares[i - 1], secret, ctx);
-//        printPoint(shares[i-1], ctx);
         
     }
     
     for (int i = 0; i < t; i++){
         BN_free(coeffs[i]);
     }
+    
     BN_free(peval);
     BN_free(pterm);
     BN_free(base);
     BN_free(exp);
     BN_CTX_free(ctx);
+    
 }
 
 void lagX(BIGNUM *prod, int shareIndexes[], int length, int i, BN_CTX *ctx){
+    
     BIGNUM *numerator = BN_new();
     BIGNUM *a = BN_new();
     BIGNUM *b = BN_new();
@@ -83,29 +71,13 @@ void lagX(BIGNUM *prod, int shareIndexes[], int length, int i, BN_CTX *ctx){
             continue;
         }
         
-        printf("i: %d, j: %d:\n",i,j);
-        
-//        int intNumerator = 0 - shareIndexes[j];
-//        int intDenominator = shareIndexes[i] - shareIndexes[j];
-//        printf("num: %d, den: %d\n",intNumerator,intDenominator);
-        
         BN_set_word(a, 0);
         BN_set_word(b, shareIndexes[j]);
         BN_sub(numerator, a,b);
         BN_set_word(a, shareIndexes[i]);
         BN_set_word(b, shareIndexes[j]);
         BN_sub(denominator, a, b);
-        //test to see if geeting rid of negative numbers resolves modinv issue:
-        //BN_add(denominator, denominator, get0Order());
-        printf("inv of ");
-        printBN(denominator);
-        printf("mod ");
-        printBN(get0Order());
-        printf("is");
         BN_mod_inverse(denominator, denominator, get0Order(), ctx);
-        printBN(denominator);
-        //ok, modinv behaves differently in the different libraries. might be sue to negative numbers?
-        printBN(denominator);
         BN_mod_mul(fraction, numerator, denominator, get0Order(), ctx);
         BN_mod_mul(prod, prod, fraction, get0Order(), ctx);
         
@@ -137,12 +109,7 @@ EC_POINT* gShamirReconstruct(EC_POINT *shares[], int shareIndexes[], int t, int 
     
     for (int i = 0; i < length; i++) {
         
-        printf("reconstructing for share i = %d ",i);
-        printPoint(shares[i], ctx);
-        printf("shareindex: %d\n", shareIndexes[i]);
         lagX(lagrangeProd, shareIndexes, length, i, ctx);
-        printf("lag: ");
-        printBN(lagrangeProd);
         EC_POINT *term = multiply(shares[i], lagrangeProd);//consider refactor to make multiply take result as an input pointer, to allow reuse and avoid multiple allocations
         EC_POINT_add(get0Group(), sum, sum, term, ctx);
         EC_POINT_free(term);
--- OpenSSL-for-iOS/ShamirP256.m
+++ OpenSSL-for-iOS/ShamirP256.m
@@ -21,117 +21,6 @@
     
     clock_t start_time = clock();
     
-    BN_CTX *ctx = BN_CTX_new();
-    
-    //test getorder
-    const BIGNUM *order = get0Order();
-    printf("order:");
-    printBN(order);
-    
-    //test get radom Zp element
-    BIGNUM *randnum = randZp(ctx);
-    printf("randint:");
-    printBN(randnum);
-    BN_free(randnum);
-    
-    
-    //test curve multiplication
-    const int numMultiplications = 10000;//*10000;
-    const EC_POINT *gen = get0Gen();
-    BIGNUM *rand = randZp(ctx);
-    for (int i=0; i<numMultiplications; i++) {
-        multiply(gen, rand);
-    }
-    BN_free(rand);
-    BIGNUM *five = BN_new(), *six = BN_new(), *eleven = BN_new();
-    BN_dec2bn(&five, "5");
-    BN_dec2bn(&six, "6");
-    BN_dec2bn(&eleven, "11");
-    EC_POINT *pfive = multiply(gen, five);
-    EC_POINT *psix = multiply(gen, six);
-    EC_POINT *peleven = multiply(gen, eleven);
-    EC_POINT *added = EC_POINT_new(get0Group());
-    EC_POINT_add(get0Group(), added, psix, pfive, ctx);
-    
-    if(EC_POINT_cmp(get0Group(), peleven, added, ctx) == 0){
-        printf("mul works as expected\n");
-    } else {
-        printf("mul NOT working as expected\n");
-    }
-    BN_free(five);
-    BN_free(six);
-    BN_free(eleven);
-    EC_POINT_free(pfive);
-    EC_POINT_free(psix);
-    EC_POINT_free(peleven);
-    EC_POINT_free(added);
-    
-    //test modp
-    printf("modadd stuff \n");
-    BIGNUM *a = BN_new(), *b = BN_new(), *c = BN_new(), *r = BN_new(), *tmp = BN_new();
-    //    BN_dec2bn(&a, "3");
-    //    BN_dec2bn(&b, "3");
-    //    BN_dec2bn(&c, "5");
-    BN_set_word(a, 3);
-    BN_set_word(b, 3);
-    BN_set_word(c, 5);
-    
-    BN_mod_add(r, a, b, c, ctx);
-    printf("r (expected 1): \t\t:");
-    printBN(r);
-    
-    
-    BN_add(tmp, a, get0Order()); //3 + p
-    BN_mod(r, tmp, get0Order(), ctx);// 3+p mod p
-    printf("r (expected 3): \t\t:");
-    printBN(r);
-    
-    BN_free(a);
-    BN_free(b);
-    BN_free(c);
-    BN_free(r);
-    BN_free(tmp);
-    BN_CTX_free(ctx);
-    
-    
-    //testmodexp
-    BIGNUM *resm = BN_new(), *base = BN_new(), *exp = BN_new(), *mod = BN_new();
-    BN_dec2bn(&base, "123");
-    BN_dec2bn(&exp, "456");
-    BN_dec2bn(&mod, "789");
-    
-    BN_CTX *ctx2 = BN_CTX_new();
-    BN_mod_exp(resm, base, exp, mod, ctx2);
-    printf("r (expected 699): \t\t:");
-    printBN(resm);
-    
-    BN_free(resm);
-    BN_free(base);
-    BN_free(exp);
-    BN_free(mod);
-    BN_CTX_free(ctx2);
-    
-    //test modinv
-    BIGNUM *resi = BN_new(), *v = BN_new(), *w = BN_new();
-    
-    BN_dec2bn(&v, "456");
-    BN_dec2bn(&w, "11");
-    
-    BN_CTX *ctx3 = BN_CTX_new();
-    BN_mod_inverse(resi, v, w, ctx3);//returns NULL if inversese foes not exist
-    printf("r (expected 9): \t\t:");
-    printBN(resi);
-    
-    BN_CTX_free(ctx3);
-    BN_free(resi);
-    BN_free(v);
-    BN_free(w);
-    
-    clock_t end_time = clock();
-    double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-    NSString *formattedString = [[NSString alloc] initWithFormat:@"Time: %f seconds\n", elapsed_time];
-    NSLog(formattedString);
-    
     //test gen shares
     const int t = 1; // t + 1 needed to reconstruct
     const int n = 3;
@@ -164,23 +53,17 @@
     
     printf("reconstructed: ");
     printPoint(reconstructed, ctx4);
-        
-    //AFTER LUNCH, TEST toPoint in both languages and see if differs. Seems fine
-//    for (int i = 1; i <= 40; i++) {
-//        BIGNUM *bn = BN_new();
-//        BN_set_word(bn, i);
-//        EC_POINT *poi = multiply(get0Gen(), bn);
-//        printf("%d : ",i);
-//        printPoint(poi, ctx4);
-//        BN_free(bn);
-//        EC_POINT_free(poi);
-//
-//    }
-        
+    
     BN_free(seven);
     BN_CTX_free(ctx4);
     EC_POINT_free(secret);
     EC_POINT_free(reconstructed);
+    
+    clock_t end_time = clock();
+    double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    NSString *formattedString = [[NSString alloc] initWithFormat:@"Time: %f seconds\n", elapsed_time];
+    NSLog(formattedString);
+    
     return @"";
 }
 
