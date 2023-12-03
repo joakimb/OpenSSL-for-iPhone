c229760537e2ea48155721884e1f37145d21a144
werekorren <paul.stankovski_wagner@eit.lth.se>
2023-12-03 13:14:53 +0100

    more test cases in main

--- OpenSSL-for-iOS/main.c
+++ OpenSSL-for-iOS/main.c
@@ -32,8 +32,8 @@ static void print_timing_vector(const char *name, const char *unit, int len, dou
 }
 
 static void test_suite_performance(int include_correctness_test) {
-    int n[] = {10,20,50,100,200,300,400,500,528,750,1000,2000,3000,4000,5000};
-    int t[] = { 5,10,25, 50,100,150,200,250,264,375, 500,1000,1500,2000,2500};
+    int n[] = {10,20,50,100,200,300,400,500,528,750,1000,2000,3000,4000,5000,7500,10000,15000,20000};
+    int t[] = { 5,10,25, 50,100,150,200,250,264,375, 500,1000,1500,2000,2500,3750, 5000, 7500,10000};
     double timing_results[10];
     double setup_and_keygen_time[sizeof(n)/sizeof(n[0])];
     double distribution_time[sizeof(n)/sizeof(n[0])];
